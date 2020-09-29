1b4aaa19d6cd0448cebde95825ec5bc85cf12e05
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-29 04:27:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,15 +12,26 @@ module FormsLab
     end
     
     post '/pirates' do 
-      pirate = params[:pirate]
-      @pirate = Pirate.new(pirate[:name], pirate[:weight], pirate[:height])
-      params[:pirate][:ships].each do |ship| 
+      pirate = Pirate.create(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:weight])
+      
+       params[:pirate][:ships].each do |ship_data| 
+         ship = Ship.new(ship_data)
+         ship.pirate = pirate
+         ship.save
+       end 
+      
+      redirect to "/pirates/#{pirate.id}"
+    end 
+    #   pirate = params[:pirate]
+    #   @pirate = Pirate.new(pirate[:name], pirate[:weight], pirate[:height])
+    #   params[:pirate][:ships].each do |ship| 
         
-        Ship.new(ship) 
-      end
-      @ships = Ship.all
+    #     Ship.new(ship) 
+    #   end
+      
+    #   @ships = Ship.all
      
-      erb :'pirates/show'
-    end
+    #   erb :'pirates/show'
+    # end
   end
-end
+end 
